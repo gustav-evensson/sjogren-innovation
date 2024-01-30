fd508b57eba56eece04617fe395269328b3bd0b1
Gustav Evensson <77409965+gustav-evensson@users.noreply.github.com>
2024-01-30 20:38:57 +0100

    fix: svg DOM prperty

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import MaxWidthProvider from "./MaxWidthProvider";
 
 export default function Footer() {
   return (
-    <footer className="bg-background_secondary py-8 sm:py-12">
+    <footer className="bg-background_secondary text-text py-8 sm:py-12">
       <MaxWidthProvider>
         <div className="flex flex-col items-center justify-between gap-12 md:flex-row md:items-start">
           <div className="flex flex-col gap-12 md:flex-row">
@@ -34,16 +34,16 @@ export default function Footer() {
                 <path
                   d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
                   stroke="#292D32"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path
                   d="M12 15.5C13.933 15.5 15.5 13.933 15.5 12C15.5 10.067 13.933 8.5 12 8.5C10.067 8.5 8.5 10.067 8.5 12C8.5 13.933 10.067 15.5 12 15.5Z"
                   stroke="#292D32"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
                 <path d="M17.6361 7H17.6477" stroke="#292D32" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
