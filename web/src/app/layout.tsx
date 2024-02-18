d06cba200b84b73f5bdca662594adca05e90094d
Gustav Evensson <77409965+gustav-evensson@users.noreply.github.com>
2024-02-18 16:49:03 +0100

    fix: metadata fixes

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -10,20 +10,14 @@ export const metadata: Metadata = {
   description:
     "Välkommen, Edvin heter jag och driver min egen lilla verkstad. Verksamheten grundar sig på produktutveckling och idégenerering. Jag pysslar med att hjälpa andra med lösningar och plastdetaljer via 3D-ritningar och 3D-skrivning. Har du en idé så hjälper jag mer än gärna till med att bolla idéer, har du något problem så skulle vi kunna se till en lösning på detta.",
   openGraph: {
-    type: "website",
-    locale: "sv_SE",
-    url: "https://sjogreninnovation.se",
+    siteName: "Sjögren Innovation",
     title: "Sjögren Innovation",
     description:
       "Välkommen, Edvin heter jag och driver min egen lilla verkstad. Verksamheten grundar sig på produktutveckling och idégenerering. Jag pysslar med att hjälpa andra med lösningar och plastdetaljer via 3D-ritningar och 3D-skrivning. Har du en idé så hjälper jag mer än gärna till med att bolla idéer, har du något problem så skulle vi kunna se till en lösning på detta.",
-    images: [
-      {
-        url: "og/thumbnail.png",
-        width: 2400,
-        height: 1260,
-        alt: "Sjögren Innovation",
-      },
-    ],
+    url: "https://sjogreninnovation.se",
+    images: "og/thumbnail.png",
+    type: "website",
+    locale: "sv_SE",
   },
 };
 
